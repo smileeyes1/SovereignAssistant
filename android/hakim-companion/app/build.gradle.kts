c82996a16f600fb2ee572761b821de004e3a24fd
smileeyes1 <smile.eyes1@gmail.com>
2026-09-12 11:42:02 +0300

    release(android): stage unified independent companion 0.4.0

--- android/hakim-companion/app/build.gradle.kts
+++ android/hakim-companion/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "org.hakim.omega.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 5
+        versionName = "0.4.0-unified-independent"
     }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions {
