31bddbb8a7a7444d3195138e39e16594610ac32f
smileeyes1 <smile.eyes1@gmail.com>
2026-09-12 12:14:01 +0300

    android: promote sovereign-local mainline 0.5.0

--- android/hakim-companion/app/build.gradle.kts
+++ android/hakim-companion/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "org.hakim.omega.companion"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0-unified-independent"
+        versionCode = 6
+        versionName = "0.5.0-sovereign-local-mainline"
     }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions {
