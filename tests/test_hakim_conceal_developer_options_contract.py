6d0cfc62f9d5bf0521a5cb64266c416721a61fbd
smileeyes1 <smile.eyes1@gmail.com>
2026-09-13 17:27:19 +0300

    اختبار: احتفظ بعقود المرحلتين دون حذف أي اسم اختبار

--- tests/test_hakim_conceal_developer_options_contract.py
+++ tests/test_hakim_conceal_developer_options_contract.py
@@ -7,13 +7,21 @@ BOOT = ROOT / 'scripts' / 'hakim-zero-burden-bootstrap.sh'
 SUP = ROOT / 'scripts' / 'hakim-multibridge-supervisor.sh'
 
 
-def test_conceal_path_changes_only_master_visibility_after_wireless_is_proven():
+def _assert_conceal_master_contract():
     s = SCRIPT.read_text(encoding='utf-8')
     assert 'WIRELESS_ADB_NOT_ENABLED' in s
     assert 'settings put global development_settings_enabled 0' in s
     assert 'adb_wifi_enabled' in s
     assert 'DEV_AFTER' in s and 'WIFI_AFTER' in s
     assert 'FIELD_VERIFIED_DEV_MASTER_OFF_WIRELESS_ADB_ON=PASS' in s
+
+
+def test_conceal_path_changes_only_master_visibility_after_wireless_is_proven():
+    _assert_conceal_master_contract()
+
+
+def test_conceal_path_changes_only_master_after_wireless_is_proven():
+    _assert_conceal_master_contract()
 
 
 def test_conceal_path_has_detached_rollback_before_mutation():
@@ -36,13 +44,21 @@ def test_guardian_is_proven_by_self_healing_master_flag():
     assert 'settings put global adb_wifi_enabled 1' in g
 
 
-def test_bootstrap_exposes_one_command_and_boot_retry_only_after_field_pass():
+def _assert_bootstrap_conceal_contract():
     s = BOOT.read_text(encoding='utf-8')
     assert 'conceal-dev)' in s
     assert 'reveal-dev)' in s
     assert 'hakim-conceal-dev-options' in s
     assert 'developer_master_off_wireless_adb_on_field_verified' in s
     assert 'hakim-boot-conceal.log' in s
+
+
+def test_bootstrap_exposes_one_command_and_boot_retry_only_after_field_pass():
+    _assert_bootstrap_conceal_contract()
+
+
+def test_bootstrap_exposes_conceal_and_reversible_release():
+    _assert_bootstrap_conceal_contract()
 
 
 def test_supervisor_restarts_guardian_only_after_field_verification():
