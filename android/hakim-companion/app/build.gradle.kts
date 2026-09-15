946f0b368faa8b55e99619acf26e4bc1c0f581ef
smileeyes1 <smile.eyes1@gmail.com>
2026-09-15 14:53:00 +0300

    حكيم: رفع compileSdk إلى 36 مع إبقاء targetSdk 35

--- android/hakim-companion/app/build.gradle.kts
+++ android/hakim-companion/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "org.hakim.omega.companion"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "org.hakim.omega.companion"
         minSdk = 26
